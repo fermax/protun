e9e8e3ab667738e9fcf209f9f47974056708e8ed
fermax <adabihbb@gmail.com>
2016-12-28 16:17:50 +0100

    Update User.php class
    تحسينات في كلاس اليوزر

--- User.php
+++ User.php
@@ -2,50 +2,118 @@
 namespace cms\core;
 include "Init.php";
 
-class User {
+class Post {
 
     private $db;
-    private $sql;
+    private $result;
 
 
     /**
      * User constructor.
      * @param Db $db
      */
-    public function __construct(Db $db) // dependency injection
+    public function __construct(Db $db)
     {
         $this->db = $db;
         return $this->db;
     }
 
 
-    public function all()
+    /**
+     * @param int $limit
+     * @param String $table_name
+     * @param String $column
+     * @param int $offset
+     * @return array|null
+     */
+    public function all($table_name, $limit = 15, $offset = 0, $column = 'id')
     {
+        $this->db->select()->from($table_name)->order_by($column, "DESC")->limit($limit, $offset);
+        $this->result = $this->db->all();
+        if (null != $this->result )
+        {
+            return $this->result;
+        }
+        else
+        {
+            return null;
+        }
+    }
 
+    /**
+     * @param $id
+     * @param String $tableName
+     * @return mixed|null
+     */
+    public function single($tableName, $id)
+    {
+        $this->db->select()->from($tableName)->where("id", "=", $id);
+        $this->result = $this->db->first();
+		
+        if ( null != $this->result )
+        {
+            return $this->result;
+        }
 
+        return null;
     }
 
 
-    public function get_data( $user_id )
+    /**
+     * @param $table
+     * @param array $postArray
+     * @return boolean
+     */
+    public function add($table , Array $postArray)
     {
-
-    }
-
-    public function add( Array $user_idata )
-    {
+        $this->db->insert($table, $postArray)->execute();
+        if( count( $this->db->getErrors() ) == 0 )
+        {
+            return true;
+        }
         
+        return false;
     }
 
 
-    public function edit( $user_id )
+    /**
+     * @param $table
+     * @param $id
+     * @param array $arrayValues
+     * @param string $condition
+     * @return bool
+     */
+
+    public function edit($table, $id, Array $arrayValues, $condition = "=")
     {
+        $this->db->update($table,$arrayValues)->where("id", $condition, $id)->execute();
+        if( count( $this->db->getErrors() ) == 0 )
+        {
+            return true;
+        }
         
+        return false;
     }
 
-    public function remove( $user_id )
+
+    /**
+     * @param $table_name
+     * @param $id
+     * @return bool|null
+     */
+    public function remove($table_name, $id)
     {
+        if(is_int($id) && $id > 0)
+        {
+            $this->db->delete($table_name)->where("id", "=", $id)->execute();
+            if( count( $this->db->getErrors() ) == 0 )
+            {
+                return true;
+            }
         
+        return false;
+        }
+        return null;
     }
-
-
+    
 }
