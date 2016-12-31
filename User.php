8888cd635f12284fe7d0cbe03e563411e659403a
fermax <adabihbb@gmail.com>
2016-12-31 19:27:53 +0100

    Update User.php class
    تحديثات متنوعة و تحسينات مهمة تخص إنشاء و تعديل و حذف مستخدم

--- User.php
+++ User.php
@@ -1,52 +1,63 @@
 <?php
 /**
-* Author: HocineFR
-* Date: 26-12-2016
-* Time: 22:30
-*/
+ * Author: HocineFR
+ * Date:   30-12-2016
+ * Time:   22:12
+ */
+namespace hocine\protun\core\user;
 
-class User extends db
+use hocine\protun\core\database\db;
+
+require_once "init.php";
+
+class User
 {
-	
     private $_salt = '$[[158#~^\@@]}}]';
-    
+    private $db;
+    //hash("sha512", $pass);
+
+
     /**
      * User constructor.
+     * @param db $db
      */
-    public function __construct()
+    public function __construct(db $db)
     {
-        parent::__construct();
+        $this->db = $db;
+        return $this->db;
     }
 
-    public function login($username, $password)
+    public function login($username, $password, $userTable = 'users')
     {
-        $query = $this->select( "users.username, users.password" )
-                      ->from( "users" )
-                      ->where( "users.username", "=" , $username )
-                      ->and_where( "users.password","=", hash("sha512",trim($password).$this->_salt ) );
+        $query = $this->db->select( $userTable."username, ".$userTable.".password" )
+            ->from( $userTable )
+            ->where( $userTable.".username", "=" , $username )
+            ->and_where( $userTable.".password","=", hash("sha512",trim($password).$this->_salt ) );
         if( $query->rowCount() > 0 )
         {
             return true;
         }
         else
         {
-            $this->setError("اسم المستخدم أو كلمة المرور غير صحيحة");
+            $this->db->setError("اسم المستخدم أو كلمة المرور غير صحيحة");
             return false;
         }
     }
 
-/************************************ admin section ***************************/
+    /************************************ admin section ***************************/
 
 
     /**
+     * @param string $userTable
+     * @param string $orderBy
      * @return array|null
      */
-    public function getAllUsers()
+    public function getAllUsers( $userTable = 'users' , $orderBy = 'id')
     {
-        $result = $this->query("SELECT * FROM `users`")->order_by("id", "DESC")->all();
+        $result = $this->db->select()->from($userTable)->order_by( $orderBy, "DESC" )->all();
         if( is_null( $result ) )
         {
-            return $this->getErrors();
+            return $this->db->getErrors();
         }
         return $result;
     }
@@ -58,23 +69,22 @@ class User extends db
      */
     public function single($id)
     {
-         $result = $this->select()->from("users")->where("id", "=", $id)->first();
-         if( is_null( $result ) )
-         {
-             return $this->getErrors();
-         }
-         return $result;
+        $result = $this->db->select()->from("users")->where("id", "=", $id)->first();
+        if( is_null( $result ) )
+        {
+            return $this->db->getErrors();
+        }
+        return $result;
 
     }
 
 
 
-     /**
+    /**
      * @param array $userData
-     * @param string $uniqueColumn
      * @return array|null|string
      */
-    public function createUser(Array $userData, $uniqueColumn = 'username')
+    public function createUser(Array $userData)
     {
         // Hashing user Password after sending it to db
         foreach ($userData as $k => $v)
@@ -85,24 +95,28 @@ class User extends db
             }
         }
         // هل المستخدم موجود مسبقا في قاعدة البيانات؟
-        $user_exist = $this->select("username")->from('users')->where("username", "=", $userData[$uniqueColumn])->rowCount();
+        $user_exist = $this->db
+                            ->select("username, email")
+                            ->from('users')
+                            ->where("username", "=", $userData['username'])->rowCount();
 
         if( $user_exist === 0 )
         {
-            $result = $this->insert('users', $userData)->execute();
+            $result = $this->db->insert('users', $userData)->execute();
             if( is_null( $result ) )
             {
-                return $this->getErrors();
+                return $this->db->getErrors();
             }
             return $result;
         }
         else
         {
-            $this->setError("This User already exist ! ");
-            return $this->getErrors();
+            $this->db->setError("This User already exist ! ");
+            return $this->db->getErrors();
         }
 
-    }
+    }// end method
+
 
     /**
      * @param $id
@@ -111,10 +125,18 @@ class User extends db
      */
     public function editUser($id, Array $userData)
     {
-        $result = $this->update('users', $userData)->where("id", "=", $id)->execute();
+        // Hashing user Password after sending it to db
+        foreach ($userData as $k => $v)
+        {
+            if( $k == "password" )
+            {
+                $userData[$k] = hash("sha512", $v.$this->_salt);
+            }
+        }
+        $result = $this->db->update('users', $userData)->where("id", "=", $id)->execute();
         if( is_null( $result ) )
         {
-            return $this->getErrors();
+            return $this->db->getErrors();
         }
         return $result;
     }
@@ -128,11 +150,12 @@ class User extends db
      */
     public function deleteUser($id)
     {
-        $result = $this->delete('users')->where('id', '=', $id)->execute();
+        $result = $this->db->delete('users')->where('id', '=', $id)->execute();
         if( is_null( $result ) )
         {
-            return $this->getErrors();
+            return $this->db->getErrors();
         }
         return $result;
     }
+
 }
