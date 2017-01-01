9c6713de097cf3c6bb5cc546c4eabea57fe53cb2
fermax <adabihbb@gmail.com>
2017-01-01 13:37:53 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+// how to...
+
+
+// import needed classes:
+use hocine\protun\core\db;
+use hocine\protun\core\User;
+
+require_once __DIR__."/vendor/autoload.php";
+
+// connect to the database
+    db::addConnection(
+        [
+            'host'    => 'localhost',
+            'user'    => 'root',
+            'pass'    => '',
+            'db'      => 'db',
+            'charset' => 'utf8'
+        ]
+    );
+
+
+// database object:
+$db = new db();
+
+// User object with dependency injection:
+$user = new User( $db );
+
+// the user table name:
+$user->setUserTable("users");
+
+// the id column
+$user->setIdColumn("id");
+
+// the unique column (( يستخدم في منع المستخدم من أن يسجل مرتين بنفس الاسم أو الايميل  ))
+$user->setUniqueColumn("username");
+
+// results order by column (( for example: id, username, created_at ...etc ))
+$user->setOrderByColumn('id');
+
+
+// user login: return --> true/false
+//$is_logged_in = $user->login("fermax", "1521980Fr");
+
+
+
+// delete a user by id:
+//$user->remove(1);
+/*
+ * you can delete a user by email; flow this two steps :
+ *                                 -1 $user->setIdColumn("email")
+ *                                 -2 $user->remove("user@example.com");
+ * */
+
+
+
+/*
+ * create a user:
+ *
+$user->create([
+                            'username'    => 'Alhocine',
+                            'password'    => '1521980Fr',
+                            'email'       => 'adabihbb@gmail.com',
+                            'fullName'    => 'Hocine Ferradj',
+                            'secret'      => 'What is my favorite number?',
+                            'answer'      => '4',
+                            'secret_hash' => Hash::Make("654321")
+                           ]);
+
+*/
+
+// edit a user:
+$query = $user->edit(7, ["email" => "fermax_hbb@yahoo.com"]);
+
+if( count( $db->getErrors())  )
+{
+    echo "<pre style='background-color: red; color: white; font-size: 20px;'>";
+    print_r( $db->getErrors() );
+    echo "</pre>";
+}
